2a5fd30363b5c7d25931e73874a8af0e1201e5cc
Khushant Wankhede <yashwankhede53@gmail.com>
2025-03-30 01:32:22 +0530

    original without commeting background color and text color

---
+++ vite-project/src/component/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import { connect } from 'react-redux'
+// rfx
+export const Header = (props) => {
+  return (
+    <header className='p-3'>
+
+    </header>
+  )
+}
+
+const mapStateToProps = (state) => ({})
+
+const mapDispatchToProps = {}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
