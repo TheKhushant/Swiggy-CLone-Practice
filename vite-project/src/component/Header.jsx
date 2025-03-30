1495869a5e8e7253f14388fb533bf0cc32122266
Khushant Wankhede <yashwankhede53@gmail.com>
2025-03-30 10:55:52 +0530

    tailwindcss lagana bhulgaya tha aur from bhi !

--- vite-project/src/component/Header.jsx
+++ vite-project/src/component/Header.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 // rfc 
 export default function Header() {
   return (
-      <header className='p-[15px] shadow-xl'>
-          <div className='max-w-[1200px] mx-auto border border-red-500'>
-              <div className='w-[80px] h-[80px] border border-blue-500'>
-                  <img src="images/logo.png" className='w-full h-full object-cover' alt="not" />
-              </div>
-          </div>
-      </header>
+    //   <header className='p-[15px] shadow-xl'>
+    //       <div className='max-w-[100px] mx-auto border border-red-500'>
+    //           <div className='w-[80px] h-[80px] border border-blue-500'>
+    //               <img src="images/logo.png" className='w-full h-full' alt="not" />
+    //           </div>
+    //       </div>
+    //   </header>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   )
 }
