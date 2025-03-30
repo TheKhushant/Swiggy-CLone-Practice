62ebd89fd56da1efa7fbe1994f5c79cb38c22ca7
Khushant Wankhede <yashwankhede53@gmail.com>
2025-03-30 23:46:24 +0530

    restart 30 march 20225

--- vite-project/src/component/Header.jsx
+++ vite-project/src/component/Header.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 // rfc 
+import { RxCaretDown } from "react-icons/rx";
+
 export default function Header() {
   return (
-    //   <header className='p-[15px] shadow-xl'>
-    //       <div className='max-w-[100px] mx-auto border border-red-500'>
-    //           <div className='w-[80px] h-[80px] border border-blue-500'>
-    //               <img src="images/logo.png" className='w-full h-full' alt="not" />
-    //           </div>
-    //       </div>
-    //   </header>
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <header className='p-[15px] shadow-xl'>
+          <div className='max-w-[1200px] mx-auto border border-red-500 flex items-center'>
+              <div className='w-[100px] '>
+                  <img src="images/logo.png" className='w-full' alt="not" />
+              </div>
+              <div className=''>
+              <span className=' font-bold border-b-[3px] border-[black]'>Nagpur,</span> Maharashtra, India <RxCaretDown fontSize={25} className='inline text-[#fc8019]'/>
+              </div>
+          </div>
+      </header>
+    
   )
 }
