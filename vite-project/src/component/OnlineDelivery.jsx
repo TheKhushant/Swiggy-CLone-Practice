9cbd0983df0f06ec2e0bd32f07fa84361e8d6624
Khushant Wankhede <yashwankhede53@gmail.com>
2025-04-11 09:02:20 +0530

    solved top restaurent display issue

--- vite-project/src/component/OnlineDelivery.jsx
+++ vite-project/src/component/OnlineDelivery.jsx
@@ -1,10 +1,11 @@
 import React, {useEffect, useState} from 'react'
+import Card from './Card';
 
 export default function OnlineDelivery() {
     const [data, setData] = useState([]);
         // const [categories, setCategory] = useState([0]);
-        const fetchCategory =async () => {
-            const response = await fetch("http://localhost:5000/top-restuarant-chains");
+        const fetchTopRestaurant =async () => {
+            const response = await fetch("http://localhost:5000/top-restaurant-chains");
             const apiData = await response.json();
             // const data = await response.json();
             setData(apiData);
@@ -14,28 +15,28 @@ export default function OnlineDelivery() {
                 fetchTopRestaurant();
             },[] 
         )
+        console.log("fetched data : ",data);
     return (
     <div>
-      <div className='max-w-[1200px] mx-auto'>
-              <div className='flex my-5 items-center justify-between'>
-                  <div className='text-[25px] font-bold'>Top Resturant Chains in Nagpur</div>
+        <div className='max-w-[1200px] mx-auto'>
+            <div className='flex my-5 items-center justify-between'>
+                <div className='text-[25px] font-bold'>Top Resturant Chains in Nagpur</div>
                     <div className='flex'>
                         
                     </div>
-                </div>
-              </div>
-              <div className="grid grid-cols-4 gap-3 ">
-                <div className="flex gap-5 overflow-hid den ">
-                  {
-                      data.map(
-                          (d,i)=>{
-                              return <Card {...d} key={i}/>
-                          }
-                      )
-                  }    
-                </div>
-              </div>
-          </div>
+            </div>
+        </div>
+        <div className="grid grid-cols-4 gap-5">
+            {
+                data.map(
+                    (d,i)=>{
+                        return <Card key={i} {...d}/>
+                    }
+                )
+            }
+        </div>
+    </div>
+    
     
   )
 }
